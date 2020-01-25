e43a60748da64e53b9035e24d4b153408e83f96a
funfunDaniel <54621371+funfunDaniel@users.noreply.github.com>
2020-01-25 15:25:59 +0700

    change style
    color

--- views/Home/Home.js
+++ views/Home/Home.js
@@ -42,28 +42,28 @@ export class Home extends React.Component {
                     {display_data}
                 </Container>
                 <Footer>
-                    <FooterTab style={{ backgroundColor: '#010001', justifyContent: "center" }}>
+                    <FooterTab style={{ backgroundColor: '#0f4c75', justifyContent: "center" }}>
                         <TouchableOpacity style={{ alignItems: "center", justifyContent: "center", }}
                             onPress={() => { this.setState({ page: 'News' }) }}
                         >
-                            <Icon name="earth" size={24} style={{ color: this.state.page == 'News' ? '#ffffff' : '#797e86', }}/>
-                            <Text style={[ styles.text_font, { color: this.state.page == 'News' ? '#ffffff' : '#797e86', } ]}>News</Text>
+                            <Icon name="earth" size={24} style={{ color: this.state.page == 'News' ? '#bbe1fa' : '#797e86', }}/>
+                            <Text style={[ styles.text_font, { color: this.state.page == 'News' ? '#bbe1fa' : '#797e86', } ]}>News</Text>
                         </TouchableOpacity>
                     </FooterTab>
-                    <FooterTab style={{ backgroundColor: '#010001', justifyContent: "center" }}>
+                    <FooterTab style={{ backgroundColor: '#0f4c75', justifyContent: "center" }}>
                         <TouchableOpacity style={{ alignItems: "center", justifyContent: "center", }}
                             onPress={() => { this.setState({ page: 'Shop' }) }}
                         >
-                            <Icon name="store" size={24} style={{ color: this.state.page == 'Shop' ? '#ffffff' : '#797e86', }}/>
-                            <Text style={[ styles.text_font, { color: this.state.page == 'Shop' ? '#ffffff' : '#797e86', } ]}>Shop</Text>
+                            <Icon name="store" size={24} style={{ color: this.state.page == 'Shop' ? '#bbe1fa' : '#797e86', }}/>
+                            <Text style={[ styles.text_font, { color: this.state.page == 'Shop' ? '#bbe1fa' : '#797e86', } ]}>Shop</Text>
                         </TouchableOpacity>
                     </FooterTab>
-                    <FooterTab style={{ backgroundColor: '#010001', justifyContent: "center" }}>
+                    <FooterTab style={{ backgroundColor: '#0f4c75', justifyContent: "center" }}>
                         <TouchableOpacity style={{ alignItems: "center", justifyContent: "center", }}
                             onPress={() => { this.setState({ page: 'Profile' }) }}
                         >
-                            <Icon name="account" size={24} style={{ color: this.state.page == 'Profile' ? '#ffffff' : '#797e86', }}/>
-                            <Text style={[ styles.text_font, { color: this.state.page == 'Profile' ? '#ffffff' : '#797e86', } ]}>Profile</Text>
+                            <Icon name="account" size={24} style={{ color: this.state.page == 'Profile' ? '#bbe1fa' : '#797e86', }}/>
+                            <Text style={[ styles.text_font, { color: this.state.page == 'Profile' ? '#bbe1fa' : '#797e86', } ]}>Profile</Text>
                         </TouchableOpacity>
                     </FooterTab>
                 </Footer>
--- views/Shop/Shop.js
+++ views/Shop/Shop.js
@@ -98,8 +98,8 @@ export default class Shop extends React.Component {
         } 
 
         return (                
-            <Content style={{ backgroundColor: '#010001', }}>
-                <Header style={{ backgroundColor: '#010001', }}>
+            <Content style={{ backgroundColor: '#3282b8', }}>
+                <Header style={{ backgroundColor: '#0f4c75', }}>
                     <ImageBackground 
                         resizeMode='cover' 
                         source={require('../../images/bghead.png')} 
@@ -109,16 +109,16 @@ export default class Shop extends React.Component {
                     </ImageBackground>
                 </Header>
                 <View style={{ padding: 12, }}>
-                    <View style={[{ height: 48, backgroundColor: '#474648', flexDirection: 'row', borderRadius: 25, elevation: 3, }]}>
+                    <View style={[{ height: 48, backgroundColor: '#0f4c75', flexDirection: 'row', borderRadius: 25, elevation: 3, }]}>
                         <TextInput style={[ styles.text_font, { flex: 1, paddingLeft: 18, }]}
                             placeholder="What are you looking for ?" 
-                            placeholderTextColor="#979697"
+                            placeholderTextColor="#bbe1fa"
                             underlineColorAndroid="transparent"
                             onChangeText={(keyword) => { this.setState({ keyword }) }}
                             value={this.state.keyword} 
                         />
                         <TouchableOpacity style={{ width: 50, height: 60, }} onPress={() => { this._fetchData() }}>
-                            <Icon name="magnify" size={28} style={{ color: '#979697', marginTop: 10, }} />
+                            <Icon name="magnify" size={28} style={{ color: '#bbe1fa', marginTop: 10, }} />
                         </TouchableOpacity>
                     </View>
                 </View>
